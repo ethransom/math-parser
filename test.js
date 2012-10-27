3bd15fcafc2fce626eef326e95f84cdc661d7349
Ethan Ransom <derethanhausen@gmail.com>
2012-10-26 22:19:15 -0600

    Rewrite test suite-- _much_ smaller now

--- test.js
+++ test.js
@@ -18,138 +18,40 @@ describe('Parser', function(){
 		});
 	}
 
-	describe('basic arithmetic', function(){
-		it('adds numbers correctly', function(done){
-			parse('2 + 2', function(result) {
-				expect(result).to.be(4);
-				done();
-			})
+	function assert_parse(exp, answer, done) {
+		parse(exp, function(result) {
+			expect(result).to.be(answer);
+			done();
 		})
+	}
+	var expressions = [
+		['2 + 2',4],
+		['5 - 3',2],
+		['5 * 6',30],
+		['12 / 4',3],
+		['5 - 10',-5],
+		['2.07 + 3.13',5.20],
+		['6.39 - 4.27',2.12],
+		['2^2',4],
+		['8^(1/3)',2],
+		['8^(2/3)',4],
+		['5 * 5 + 2',27],
+		['2 + 3 * 2',8],
+		['5 * 5 - 2',23],
+		['2 - 3 * 2',-4],
+		['5 / 5 + 2',3],
+		['2 + 6 / 3',4],
+		['5 / 5 - 2',-1],
+		['2 - 6 / 3',0],
+		['3 ^ 2 - 2', 7],
+		['4 + 2 ^ 3',12],
+		['5 ^ 2 / 5',5],
+		['34 - 8 ^ 2',-30]
+	];
 
-		it('subtracts numbers correctly', function(done){
-			parse('5 - 3', function(result) {
-				expect(result).to.be(2);
-				done();
-			})
+	expressions.forEach(function (expression) {
+		it('evaluates ' + expression[0] + ' correctly', function(done) {
+			assert_parse(expression[0], expression[1], done);
 		})
-
-		it('multiplies numbers correctly', function(done){
-			parse('5 * 6', function(result) {
-				expect(result).to.be(30);
-				done();
-			})
-		})
-
-		it('adds numbers correctly', function(done){
-			parse('12 / 4', function(result) {
-				expect(result).to.be(3);
-				done();
-			})
-		})
-
-		it('can handle negative answers', function(done) {
-			parse('5 - 10', function(result) {
-				expect(result).to.be(-5);
-				done();
-			})
-		})
-	})
-
-	describe('floating point arithmetic', function() {
-		it('adds floats', function(done){
-			parse('2.07 + 3.13', function(result) {
-				expect(result).to.be(5.20);
-				done();
-			})
-		})
-
-		it('subtracts floats', function(done){
-			parse('2.07 - 5.28', function(result) {
-				expect(result).to.be(-3.21);
-				done();
-			})
-		})
-	})
-
-	describe('exponents', function() {
-		it('handles exponents correctly', function(done) {
-			parse('2^2', function (result) {
-				expect(result).to.be(4);
-				done();
-			})
-		})
-
-		it('handles rational exponents correctly', function(done) {
-			parse('8^(1/3)', function (result) {
-				expect(result).to.be(2);
-				parse('8^(2/3)', function (result) {
-					expect(result).to.be(4);
-					done();
-				})
-			})
-		})
-	})
-
-	describe('order of operations', function() {
-		it('puts multiply before add', function(done) {
-			parse('5 * 5 + 2', function (result) {
-				expect(result).to.be(27);
-				parse('2 + 3 * 2', function (result) {
-					expect(result).to.be(8);
-					done();
-				})
-			})
-		})
-
-		it('puts multiply before subtract', function(done) {
-			parse('5 * 5 - 2', function (result) {
-				expect(result).to.be(23);
-				parse('2 - 3 * 2', function (result) {
-					expect(result).to.be(-4);
-					done();
-				})
-			})
-		})
-
-		it('puts divide before add', function(done) {
-			parse('5 / 5 + 2', function (result) {
-				expect(result).to.be(3);
-				parse('2 + 6 / 3', function (result) {
-					expect(result).to.be(4);
-					done();
-				})
-			})
-		})
-
-		it('puts divide before subtract', function(done) {
-			parse('5 / 5 - 2', function (result) {
-				expect(result).to.be(-1);
-				parse('2 - 6 / 3', function (result) {
-					expect(result).to.be(0);
-					done();
-				})
-			})
-		})
-
-		it('puts exponents before addition and subtraction', function(done) {
-			parse('3 ^ 2 - 2', function (result) {
-				expect(result).to.be(7);
-				parse('4 + 2 ^ 3', function (result) {
-					expect(result).to.be(12);
-					done();
-				})
-			})
-		})
-
-		it('puts exponents before multiplication and division', function(done) {
-			parse('5 ^ 2 / 5', function (result) {
-				expect(result).to.be(5);
-				parse('34 - 8 ^ 2', function (result) {
-					expect(result).to.be(-30);
-					done();
-				})
-			})
-		})
-	})
-
+	});
 })
